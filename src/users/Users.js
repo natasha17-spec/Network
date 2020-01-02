ecf46105b4892209188dbd4640f40cab8b530cc1
natasha17 <17061990@inbox.ru>
2020-01-02 21:42:10 +0300

    54lesson_componentDidMount

--- src/users/Users.js
+++ src/users/Users.js
@@ -5,13 +5,11 @@ import userPhoto from '../users/images.png'
 
 
 class Users extends React.Component {
- constructor(props) {
-    super(props);
-
+ componentDidMount() {
          axios.get("https:social-network.samuraijs.com/api/1.0/users/").then(response => {
              this.props.setUsers(response.data.items)
-          }); }
-
+         });
+     }
 
     render  ()  {
         return <div className='usersContainer'>
