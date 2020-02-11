e331c1f6a9513d621f3a865cd18df2f75e56e85d
natasha17 <17061990@inbox.ru>
2020-02-11 22:25:14 +0300

    74 -  componentDidUpdate

--- src/components/Profile/ProfileStatus.jsx
+++ src/components/Profile/ProfileStatus.jsx
@@ -29,6 +29,15 @@ class ProfileStatus extends React.Component {
 
     };
 
+    componentDidUpdate(prevProps, prevState) {
+
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+    }
+
     render() {
         return (
             <>
