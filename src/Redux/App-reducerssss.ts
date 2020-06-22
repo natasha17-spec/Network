ec417eae93a6386e5ae26a4e3a3319697b720f77
natasha17-spec <472682289popsik>
2020-06-22 21:19:48 +0300

    typeScript - change

---
+++ src/Redux/App-reducerssss.ts
@@ -0,0 +1,42 @@
+import {authMe} from "./AuthReducer";
+import {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {AppStateType} from "./redux-store";
+
+const INITIALIZED_SUCCESS = 'INITIALIZED_SUCCESS';
+
+type InitialStateType = {
+    initialized:boolean
+}
+let initialState = {
+    initialized: false
+};
+
+const appReducer = (state:InitialStateType = initialState, action:InitializedSuccessType) => {
+    if (action.type === INITIALIZED_SUCCESS) {
+        return {
+            ...state,
+            initialized: true
+        };
+    } else {
+        return state
+    }
+};
+//*Action creators type
+type InitializedSuccessType = {
+    type: typeof INITIALIZED_SUCCESS
+}
+//*Action creators
+export const initializedSuccess = (): InitializedSuccessType => ({type: INITIALIZED_SUCCESS});
+//*Общий
+type ThunkType = ThunkAction<void, AppStateType, unknown, InitializedSuccessType>
+type ThunkDispatchType = ThunkDispatch<AppStateType, unknown, InitializedSuccessType>
+//*Thunk
+export const initializeApp = (): ThunkType => (dispatch: ThunkDispatchType) => {
+    let promise = dispatch(authMe());
+    Promise.all([promise])
+        .then(() => {
+            dispatch(initializedSuccess());
+        });
+}
+
+export default appReducer;
