536afb43ee9857f5e5864767a5665fb8a1ea12d1
natasha17-spec <17061990@inbox.ru>
2020-07-01 08:39:21 +0300

    typeScript - change URL

---
+++ src/components/Navbar/URL/URL.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import s from './URL.module.css';
+// @ts-ignore
+import {NavLink} from "react-router-dom";
+
+
+const URL = () => {
+
+        return (
+        <nav className={s.nav}>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/profile" activeClassName={s.activeLink}> Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/dialogs" activeClassName={s.activeLink}> Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/news" activeClassName={s.activeLink}> News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/users" activeClassName={s.activeLink}> Users</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink>
+            </div>
+        </nav>
+)
+};
+export default URL;
