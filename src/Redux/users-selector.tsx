318c6bd72f3d40dfe80b242ef4ff74ae921f559e
natasha17-spec <17061990@inbox.ru>
2020-07-15 23:12:33 +0300

    samurai_v2 - 04. TypeScript change - UseSelector.ts

--- src/Redux/users-selector.tsx
+++ src/Redux/users-selector.tsx
@@ -1,27 +1,28 @@
 import {createSelector} from "reselect";
+import {AppStateType} from "./redux-store";
 
-const getUsersSelector = (state) => {
+const getUsersSelector = (state:AppStateType) => {
     return state.userPage.users
 }
 export const getsUsers = createSelector(
     getUsersSelector,(users) => {
         return users.filter(u => true)})
 
-export const getPageSize = (state) => {
+export const getPageSize = (state:AppStateType) => {
     return state.userPage.pageSize;
 };
 
-export const getTotalUsersCount = (state) => {
+export const getTotalUsersCount = (state:AppStateType) => {
     return state.userPage.totalUsersCount;
 };
 
-export const getCurrentPage = (state) => {
+export const getCurrentPage = (state:AppStateType) => {
     return state.userPage.currentPage;
 };
 
-export const getIsFetching = (state) => {
+export const getIsFetching = (state:AppStateType) => {
     return state.userPage.isFetching;
 };
-export const getFollowingInProgress = (state) => {
+export const getFollowingInProgress = (state:AppStateType) => {
     return state.userPage.followingInProgress;
 };
