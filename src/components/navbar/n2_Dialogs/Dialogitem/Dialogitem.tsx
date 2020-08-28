e8218883faa4e0eeb8c3efdc39d38a1c560635bd
natasha17-spec <17061990@inbox.ru>
2020-08-28 08:30:39 +0300

    samurai_v2 - 111. refactor Dialogitem.tsx

--- src/components/navbar/n2_Dialogs/Dialogitem/Dialogitem.tsx
+++ src/components/navbar/n2_Dialogs/Dialogitem/Dialogitem.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import p from './../Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
-
-const DialogItem = (props) => {
+type PropsType = {
+    id:number
+    name:string
+}
+const DialogItem:React.FC<PropsType> = (props) => {
     let path = "/dialogs/" + props.id;
     return <div className={p.dialog + ' ' + p.active}>
         <NavLink to={path}>{props.name}</NavLink>
