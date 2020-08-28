527ae94a45336c55483ddaaf964a93c0a827d306
natasha17-spec <17061990@inbox.ru>
2020-08-28 08:24:47 +0300

    samurai_v2 - 111. refactor auth-api.ts

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -4,7 +4,6 @@ type MeResponseType = {
     id: number,
     email: string,
     login: string
-
 }
 type loginResponseType = {
      userId: number
@@ -12,15 +11,15 @@ type loginResponseType = {
 
 export const authAPI = {
     me() {
-        return instance.get<APIResponseType<MeResponseType>>(`auth/me`);
+        return instance.get<APIResponseType<MeResponseType>>(`auth/me`).then(res => res.data);;
     },
-    login(email: null | string, password: string, rememberMe = false, captcha: null | string = null) {
+    login(email: string, password: string, rememberMe = false, captcha: null | string = null) {
         return instance.post<APIResponseType<loginResponseType, ResultCodeEnum | ResultCodeForCaptchaEnum>>(`auth/login`, {
             email,
             password,
             rememberMe,
             captcha
-        });
+        }).then(res => res.data);
     },
     logout() {
         return instance.delete(`auth/login`);
