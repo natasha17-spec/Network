8feae2eb4123b8444ed91f7a94546639de9ef293
natasha17-spec <17061990@inbox.ru>
2020-08-30 16:21:13 +0300

    samurai_v2 - 111. TS withAuthRedirect.tsx

---
+++ src/hoc/withSuspense.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Preloader from "../components/common/Preloader/Preloader";
+
+
+export function withSuspense <WSP>(WrappedComponent:React.ComponentType<WSP>) {
+    return (props:WSP) => {
+        return <React.Suspense fallback={<Preloader />} >
+            <WrappedComponent {...props} />
+        </React.Suspense>
+    };
+}
