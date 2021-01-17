cf9dd1ad54e8d9827e8e56f4f314cc1f9764dd8b
natasha17-spec <17061990@inbox.ru>
2021-01-17 21:28:27 +0300

    113 - lesson refactoring values for formik

--- src/components/users/UsersSearchForm.tsx
+++ src/components/users/UsersSearchForm.tsx
@@ -10,18 +10,27 @@ const usersSearchFormValidate = (values: any) => {
     const errors = {};
     return errors;
 }
+type FormType = {
+    term: string
+    friend: "true" | "false" | "null"
+}
 
 export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
-    const submit = (values: FilterType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
+    const submit = (values: FormType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
+        const filter: FilterType = {
+            term: values.term,
+            friend: values.friend === "null" ? null : values.friend === "true"
+                ? true : false
+        }
         debugger
-        props.onFilterChanged(values)
+        props.onFilterChanged(filter)
         setSubmitting(false)
     }
 
     return (
         <div>
             <Formik
-                initialValues={{term: '',friend:null}}
+                initialValues={{term: '', friend: "null"}}
                 validate={usersSearchFormValidate}
                 onSubmit={submit}
             >
@@ -30,8 +39,8 @@ export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
                         <Field type="text" name="term"/>
                         <Field name="friend" as="select">
                             <option value="null">All</option>
-                            <option value="true">Follow</option>
-                            <option value="false">Unfollow</option>
+                            <option value="true">Friends</option>
+                            <option value="false">Not friends</option>
                         </Field>
 
                         <button type="submit" disabled={isSubmitting}>
