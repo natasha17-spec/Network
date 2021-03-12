c3a6f4fcba7eb09c8b807e867cadf13fee804b51
natasha17-spec <17061990@inbox.ru>
2021-03-12 09:28:03 +0300

    117 - add untD

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import 'antd/dist/antd.css';
 import './App.css';
-import {Redirect, Route, Switch, withRouter} from "react-router-dom";
+import {Link, NavLink, Redirect, Route, Switch, withRouter} from "react-router-dom";
 
 import {Login} from "./components/login/login";
 import {connect} from "react-redux";
@@ -9,12 +10,14 @@ import {initializeApp} from "./redux/App-reducer";
 import News from "./components/navbar/n4_News/News";
 import Music from "./components/navbar/n3_Music/Music";
 import Settings from "./components/navbar/n5_Settings/Settings";
-import Path from "./components/navbar/n1_Path/Path";
 import {AppStateType} from "./redux/redux-store";
-import HeaderContainer from "./components/header/HeaderContainer";
 import {withSuspense} from "./hoc/withSuspense";
 import Preloader from "./components/common/preloader/Preloader";
 import {UsersPage} from "./components/users/UsersPage";
+import {Breadcrumb, Button, Layout, Menu} from "antd";
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import s from "./components/navbar/n1_Path/Path.module.css";
+
 
 const DialogsContainer = React.lazy(() => import('./components/navbar/n2_Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
@@ -43,33 +46,102 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
         if (!this.props.initialized) {
             return <Preloader/>
         }
-
+        const {SubMenu} = Menu;
+        const {Header, Content, Footer, Sider} = Layout;
         return (
 
+            <Layout>
+                <Header className="header">
+                    <div className="logo"/>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                        <Menu.Item key="1"><Link to="/users"> Developers</Link></Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{padding: '0 50px'}}>
+                    <Breadcrumb style={{margin: '16px 0'}}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Layout className="site-layout-background" style={{padding: '24px 0'}}>
+                        <Sider className="site-layout-background" width={200}>
+                            <Menu
+                                mode="inline"
+                                defaultSelectedKeys={['1']}
+                                defaultOpenKeys={['sub1']}
+                                style={{height: '100%'}}
+                            >
+                                <SubMenu key="sub1" icon={<UserOutlined/>} title="My Profile">
+                                    <Menu.Item key="1">
+                                        <Link to="/profile">Profile</Link>
+                                    </Menu.Item>
+                                    <Menu.Item key="2">
+                                        <Link to="/dialogs">Messages</Link>
+                                    </Menu.Item>
+                                </SubMenu>
+                                <SubMenu key="sub2" icon={<LaptopOutlined/>} title="Developer">
+                                    <Menu.Item key="5">
+                                        <Link to="/users"> Users</Link></Menu.Item>
+                                    <Menu.Item key="6">option6</Menu.Item>
+                                    <Menu.Item key="7">option7</Menu.Item>
+                                    <Menu.Item key="8">option8</Menu.Item>
+                                </SubMenu>
+                                <SubMenu key="sub3" icon={<NotificationOutlined/>} title="subnav 3">
+                                    <Menu.Item key="9">option9</Menu.Item>
+                                    <Menu.Item key="10">option10</Menu.Item>
+                                    <Menu.Item key="11">option11</Menu.Item>
+                                    <Menu.Item key="12">option12</Menu.Item>
+                                </SubMenu>
+                            </Menu>
+                        </Sider>
+                        <Content style={{padding: '0 24px', minHeight: 280}}>
+                            <Switch>
+                                <Route exact path='/' render={() => <Redirect to={"/profile"}/>}/>
+                                <Route path='/dialogs'
+                                       render={() => <SuspendedDialogs/>}/>
+                                <Route path='/profile/:userId?'
+                                       render={() => <SuspendedProfile/>}/>
+                                <Route path='/news' render={() => <News/>}/>
+                                <Route path='/music' render={() => <Music/>}/>
+                                <Route path='/settings' render={() => <Settings/>}/>
+                                <Route path='/users' render={() => <UsersPage/>}/>
+                                <Route path='/login' render={() => <Login/>}/>
+                                <Route path='*' render={() => <div>
+                                    <Button>OK</Button> 404 not found
+                                </div>}/>
+                            </Switch>
 
-            <div className='app-wrapper'>
-                <div className='null'></div>
-                <HeaderContainer/>
 
-
-                <Path/>
-                <div className='app-wrapper-content'>
-                    <Switch>
-                        <Route exact path='/' render={() => <Redirect to={"/profile"}/>}/>
-                        <Route path='/dialogs'
-                               render={() => <SuspendedDialogs/>}/>
-                        <Route path='/profile/:userId?'
-                               render={() => <SuspendedProfile/>}/>
-                        <Route path='/news' render={() => <News/>}/>
-                        <Route path='/music' render={() => <Music/>}/>
-                        <Route path='/settings' render={() => <Settings/>}/>
-                        <Route path='/users' render={() => <UsersPage/>}/>
-                        <Route path='/login' render={() => <Login/>}/>
-                        <Route path='*' render={() => <div>404 not found</div>}/>
-                    </Switch>
-                </div>
-                <div className='null2'></div>
-            </div>
+                        </Content>
+                    </Layout>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+            // <div className='app-wrapper'>
+            //     <div className='null'></div>
+            //     <HeaderContainer/>
+            //
+            //
+            //     <Path/>
+            //     <div className='app-wrapper-content'>
+            //         <Switch>
+            //             <Route exact path='/' render={() => <Redirect to={"/profile"}/>}/>
+            //             <Route path='/dialogs'
+            //                    render={() => <SuspendedDialogs/>}/>
+            //             <Route path='/profile/:userId?'
+            //                    render={() => <SuspendedProfile/>}/>
+            //             <Route path='/news' render={() => <News/>}/>
+            //             <Route path='/music' render={() => <Music/>}/>
+            //             <Route path='/settings' render={() => <Settings/>}/>
+            //             <Route path='/users' render={() => <UsersPage/>}/>
+            //             <Route path='/login' render={() => <Login/>}/>
+            //             <Route path='*' render={() => <div>
+            //             <Button>OK</Button>    404 not found
+            //             </div>}/>
+            //         </Switch>
+            //     </div>
+            //     <div className='null2'></div>
+            // </div>
         );
     }
 }
